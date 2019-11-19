93d5886b3c1aab41686abef27f207f675444f181
Julian Camilo <julian20141016@gmail.com>
2019-11-18 22:11:50 -0500

    Proyecto Domotica y uso del KeyPad

--- DomoticaProyect/DomoticaProyect.ino
+++ DomoticaProyect/DomoticaProyect.ino
@@ -25,6 +25,73 @@ byte ene[8] = {
   B10001,
   B10001
 };
+
+byte a[8] = {
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000,
+        B10000
+};
+
+byte b[8] = {
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000,
+        B11000
+};
+
+byte c[8] = {
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100,
+        B11100
+};
+
+byte d[8] = {
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110
+};
+
+byte e[8] = {
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111,
+        B11111
+};
+
+
+byte exclama[8] = {
+        B00000,
+        B00100,
+        B00000,
+        B00000,
+        B00100,
+        B00100,
+        B00100,
+        B00100
+};
  
 const byte rowPins[rowsCount] = { 8, 2, 3, 4 };
 const byte columnPins[columsCount] = { 5, 6, 7};
@@ -49,6 +116,12 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Ingrese la clave");
+  lcd.createChar(1,a);
+  lcd.createChar(2,b);
+  lcd.createChar(3,c);
+  lcd.createChar(4,d);
+  lcd.createChar(5,e);
+  lcd.createChar(6,exclama);
 }
 
 bool verificarClave(char claveIngresada[]){
@@ -62,6 +135,37 @@ bool verificarClave(char claveIngresada[]){
 
 void iniciarSistema(){
   Serial.print("Sistema iniciado");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Arrancando");
+  lcd.setCursor(0,1);
+  lcd.print("sistema");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.write("Cargando...");
+  llenarCuadro();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.write(6);
+  lcd.write("Bienvenido!");
+  lcd.setCursor(0,1);
+  while(true){
+    key = keypad.getKey();
+    if(key){
+      }
+    }
+  
+  }
+
+void llenarCuadro(){
+  for(int cubo=0;cubo<=15;cubo++){
+    for(int i=1;i<=5;i++){
+      lcd.setCursor(cubo,1);
+      lcd.write(i);
+      delay(80);
+    }
+   }  
   }
 
 void limpiarInferior(){
